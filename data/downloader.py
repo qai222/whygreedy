7be1c0c894f396ac90301953fbf0417b764cd027
qai <qai1@fordham.edu>
2022-10-06 06:08:38 -0400

    add back apikey

---
+++ data/downloader.py
@@ -0,0 +1,14 @@
+from pymatgen.ext.matproj import MPRester
+
+from whygreedy.utils import json_dump
+
+if __name__ == '__main__':
+    mat_api_key = '***REMOVED***'
+
+    mpr = MPRester(mat_api_key)
+
+    all_compounds = mpr.query({}, properties=["task_id", "pretty_formula", 'e_above_hull',
+                                              'elements', 'volume', 'formation_energy_per_atom', 'band_gap',
+                                              'nsites', 'unit_cell_formula', 'energy_per_atom'])
+
+    json_dump(all_compounds, "mp.json.gz")
